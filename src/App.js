34cee882a3f53ecc0deae4f8964911b6f1702996
Paul Borawski <paulborawski14@gmail.com>
2019-03-15 17:25:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    name: ''
+  }
+
+  changeTheName = (e) => {
+    this.setState({ name: e.target.value })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input onChange={this.changeTheName} value={this.state.name} />
+        <div className="textOne">{this.state.name}</div>
       </div>
     );
   }
